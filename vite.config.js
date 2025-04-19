ab999a874c3f56b480b5bbf18aabcd3d92afda87
abel <abeloabate01@gmail.com>
2025-04-19 16:28:13 +0300

    2

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // vite.config.j
+  base: '/proxt/', // Replace 'broke' with your repo name
+  build: {
+    outDir: 'dist', // Ensure the build output is to the 'dist' folder
+  },
+
+
   plugins: [react()],
-  server: {},
 })
